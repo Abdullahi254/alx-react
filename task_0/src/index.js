7247ca60bacbf888b6f7820cd314128936cde120
Abdullahi254 <abdush504@gmail.com>
2024-01-29 16:29:55 +0300

    basic webpack setup

---
+++ task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+$(function () {
+    // Add three different paragraphs to the body
+    $('body').append('<p>This is the first paragraph 1.</p>');
+    $('body').append('<p>This is the second paragraph 2.</p>');
+    $('body').append('<p>This is the third paragraph 3.</p>');
+});
