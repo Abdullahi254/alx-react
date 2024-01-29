e84061abb0e88131c7384ad7376c5f19a2569369
Abdullahi254 <abdush504@gmail.com>
2024-01-29 16:47:21 +0300

    added babel loader

--- task_0/webpack.config.js
+++ task_0/webpack.config.js
@@ -7,9 +7,23 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins:[
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack Output",
-          }),
+        }),
     ]
 };
